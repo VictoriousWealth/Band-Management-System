aa3b94d8a94d64250568d1517ee1c1b023cd35ab
Edesiri <ohwomadodezri@gmail.com>
2024-11-12 10:20:10 +0000

    Member registration implemented

--- team28/src/main/java/uk/ac/sheffield/team28/team28/controller/AuthController.java
+++ team28/src/main/java/uk/ac/sheffield/team28/team28/controller/AuthController.java
@@ -1,18 +1,18 @@
 package uk.ac.sheffield.team28.team28.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import jakarta.validation.Valid;
 import uk.ac.sheffield.team28.team28.dto.MemberRegistrationDto;
 import uk.ac.sheffield.team28.team28.service.MemberService;
 
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthController {
     private final MemberService memberService;
@@ -21,19 +21,28 @@ public class AuthController {
         this.memberService = memberService;
     }
 
-   @PostMapping("/register")
-    public ResponseEntity<String> registerMember(@RequestBody MemberRegistrationDto registrationDto) {
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("member", new MemberRegistrationDto());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String performRegister(@Valid @ModelAttribute("member") MemberRegistrationDto registrationDto, 
+                                  BindingResult result, 
+                                  Model model) {
+        if (result.hasErrors()) {
+            return "register"; 
+        }
+
         try {
             memberService.registerMember(registrationDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Member registered successfully.");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Registration failed: " + e.getMessage());
+            model.addAttribute("errorMessage", e.getMessage());
+            return "register"; 
         }
-    }
 
-    @GetMapping("test")
-    public String testEndpoint() {
-        return "Worked";
+        model.addAttribute("successMessage", "Registration completed successfully, please login.");
+        return "/login"; 
     }
-
 }
