d070a721d62663f0777107f7fe0228048f85be5a
Edesiri <ohwomadodezri@gmail.com>
2024-11-13 13:54:57 +0000

    Home page for sludgate

--- team28/src/main/java/uk/ac/sheffield/team28/team28/controller/HomeController.java
+++ team28/src/main/java/uk/ac/sheffield/team28/team28/controller/HomeController.java
@@ -1,23 +1,22 @@
 package uk.ac.sheffield.team28.team28.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.ac.sheffield.team28.team28.service.MemberService;
 
-@RestController()
+import uk.ac.sheffield.team28.team28.dto.MemberRegistrationDto;
+
+@Controller()
 @RequestMapping("/")
 public class HomeController {
-    private final MemberService memberService;
 
-    public HomeController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     @GetMapping("/")
-    public ResponseEntity<String> home() {
-        return ResponseEntity.ok("This is our Home Page");
+    public String showHomePage(Model model) {
+        model.addAttribute("member", new MemberRegistrationDto());
+        return "home";
     }
 
 }
