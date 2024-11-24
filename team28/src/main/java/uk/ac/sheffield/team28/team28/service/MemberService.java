ced571b57e8edca327489c7006b5ec7b7dffd9ad
Tom Craig <twcraig1@sheffield.ac.uk>
2024-11-24 22:14:03 +0000

    Added feature where committee members can view instruments.

--- team28/src/main/java/uk/ac/sheffield/team28/team28/service/MemberService.java
+++ team28/src/main/java/uk/ac/sheffield/team28/team28/service/MemberService.java
@@ -1,5 +1,7 @@
 package uk.ac.sheffield.team28.team28.service;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -85,7 +87,16 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-
+    public Member findMember(){
+        String email;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails){
+            email = ((UserDetails) principal).getUsername();
+        } else {
+            email = principal.toString();
+        }
+        return memberRepository.findByEmail(email).get();
+    }
 
 
 }
