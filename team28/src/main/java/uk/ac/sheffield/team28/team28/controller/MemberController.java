d59d5933a0cdde374c78a420e7a2653ce3f4e2c9
Nick Efe Oni <neoni1@sheffield.ac.uk>
2024-11-25 12:46:12 +0000

    Changes to top bar components and re routing

--- team28/src/main/java/uk/ac/sheffield/team28/team28/controller/MemberController.java
+++ team28/src/main/java/uk/ac/sheffield/team28/team28/controller/MemberController.java
@@ -2,12 +2,14 @@ package uk.ac.sheffield.team28.team28.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import uk.ac.sheffield.team28.team28.service.MemberService;
+import uk.ac.sheffield.team28.team28.model.Member;
 
 
 @RestController()
-@RequestMapping("/")
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
     private final static Logger logger = LoggerFactory.getLogger(MemberController.class);
@@ -16,6 +18,11 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/account-info")
+    public String accountInfo(Model model) {
+//        model.addAttribute("member", );
+        return "account-info";
+    }
 
 
 }
