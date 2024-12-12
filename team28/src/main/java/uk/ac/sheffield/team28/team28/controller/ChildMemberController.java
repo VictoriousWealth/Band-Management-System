01338dc6740854d35d04f532451e0af29f5386c3
Edesiri <ohwomadodezri@gmail.com>
2024-12-12 13:58:43 +0000

    Fixed director permissions

--- team28/src/main/java/uk/ac/sheffield/team28/team28/controller/ChildMemberController.java
+++ team28/src/main/java/uk/ac/sheffield/team28/team28/controller/ChildMemberController.java
@@ -4,22 +4,10 @@ package uk.ac.sheffield.team28.team28.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import uk.ac.sheffield.team28.team28.model.BandInPractice;
-import uk.ac.sheffield.team28.team28.model.ChildMember;
-import uk.ac.sheffield.team28.team28.model.Member;
 import uk.ac.sheffield.team28.team28.service.ChildMemberService;
 import uk.ac.sheffield.team28.team28.service.MemberService;
-
-import java.security.Principal;
-import java.util.List;
-import java.util.Objects;
 
 
 @Controller()
@@ -27,12 +15,10 @@ import java.util.Objects;
 
 public class ChildMemberController {
     private final ChildMemberService childMemberService;
-    private final MemberService memberService;
 
 
     public ChildMemberController(ChildMemberService childMemberService, MemberService memberService) {
         this.childMemberService = childMemberService;
-        this.memberService = memberService;
 
 
     }
